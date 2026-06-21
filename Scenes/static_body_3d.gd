ab93cd5293ff58b8d7eec1ea9f913a5645db89a5
Mikingzly <mbostick@highpoint.edu>
2026-06-21 17:45:29 -0400

    Shaders work now

--- Scenes/static_body_3d.gd
+++ Scenes/static_body_3d.gd
@@ -9,4 +9,5 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	rotate_y(speed * delta)
-@export var speed:float = 5 
+@export var speed:float = 2
+ 
