36852feaf7afce0d9675079da7ea3664be7f784a
Mikingzly <mbostick@highpoint.edu>
2026-06-22 18:32:11 -0400

    Player movement changes as well as a working pause menu

--- Scripts/character_body_3d.gd
+++ Scripts/character_body_3d.gd
@@ -4,6 +4,21 @@ extends CharacterBody3D
 const SPEED = 10.0
 const JUMP_VELOCITY = 4.5
 
+@onready var head: Node3D = %Head
+@onready var camera_3d: Camera3D = $Head/Camera3D
+@export var sensitivity: float = 0.002
+@onready var pause_menu: Control = %PauseMenu
+
+func _ready() -> void:
+	camera_3d.current = true
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		head.rotate_y(-event.relative.x * sensitivity)
+		camera_3d.rotate_x(-event.relative.y * sensitivity)
+		camera_3d.rotation.x = clamp(camera_3d.rotation.x, deg_to_rad(-90), deg_to_rad(90))
+
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -17,7 +32,7 @@ func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("left", "right", "up", "down")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var direction := (head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
@@ -26,3 +41,11 @@ func _physics_process(delta: float) -> void:
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 
 	move_and_slide()
+	
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed('esc') and pause_menu.visible == false:
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+		pause_menu.show()
+	elif Input.is_action_just_pressed('esc') and pause_menu.visible == true:
+		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+		pause_menu.hide()
--- Scripts/pause_menu.gd
+++ Scripts/pause_menu.gd
@@ -1,12 +1,17 @@
 extends Control
 
+func _ready() -> void:
+	hide()
+
 func resume():
 	get_tree().paused = false
-	$AnimationPlayer.play_backwards("blur")
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	hide()
+
 
 func pause():
 	get_tree().paused = true
-	$AnimationPlayer.play("blur")
+
 
 func testEsc():
 	if Input.is_action_just_pressed("esc") and get_tree().paused == false:
